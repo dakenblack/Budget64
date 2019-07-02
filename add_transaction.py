8b02723e926cd064b932389c9c8a29fc95116498
Jabez Wilson <jabez.wilson0@gmail.com>
2019-07-02 21:51:35 +1000

    Added more goodies

---
+++ add_transaction.py
@@ -0,0 +1,51 @@
+"""
+Author  : Jabez Wilson (jwilson@iesys.com)
+Created : 02/07/2019
+
+Adds new entries
+
+TODO
+    - 
+
+CHANGELOG
+    dd/mm/2019 xx:00 - Created File 
+"""
+import pandas as pd
+import numpy as np
+
+tdata = pd.read_pickle('Transaction.pkl')
+print(tdata.tail())
+GROCERIES = 'Groceries'
+TRANSPORT = 'Transport'
+UTILITIES = 'Utilities'
+HOUSE     = 'House'
+CASH      = 'Cash'
+EATOUT    = 'Eat-Out'
+SHOPPING  = 'Shopping'
+
+REGULAR = 'Regular'
+NOTAG   = np.nan
+
+date = '2019-07-02'
+writeout = False
+
+#%%
+data = [
+#        [date, 20, TRANSPORT, REGULAR, 'Opal'],
+#        [date, 8.14, SHOPPING, NOTAG, 'Vultr'],
+#        [date, 8, GROCERIES, NOTAG, 'Coles'],
+        ]
+
+#%%
+for row in data:
+    tdata = tdata.append(pd.Series([np.datetime64(row[0])] + row[1:], index=tdata.columns), ignore_index=True)
+
+#%%
+print('Modifications:')
+print(tdata.tail())
+
+#%%
+# WARNING!! Make certain!
+if writeout:
+    print('WARNING: Writing Out!!')
+    tdata.to_pickle('Transaction.pkl')
--- visualise.py
+++ visualise.py
@@ -15,6 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.core.defchararray import add as npStrAdd
 tdata = pd.read_pickle('Transaction.pkl')
+bdata = pd.read_pickle('Budget.pkl')
 months = np.array(['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 
 
@@ -50,3 +51,28 @@ fig, ax = plt.subplots()
 ax = grouped.plot.bar(x='Date', y='Amt', rot=0)
 for p in ax.patches:
     ax.annotate(str(p.get_height()) + '$', (p.get_x()+0.04, p.get_height() - 15), c='white')
+    
+#%%
+# Comparing with the budget
+month = 7
+year = 2019
+
+tmask = (tdata['Date'].dt.year == year) & (tdata['Date'].dt.month == month)
+bmask = (bdata['Month'].dt.year == year) & (bdata['Month'].dt.month == month)
+df = tdata[tmask]
+
+budget = bdata[bmask]['Budget']
+budget.index = pd.CategoricalIndex(bdata[bmask]['Category'])
+
+cats = df['Category'].unique().astype('str')
+total = np.zeros(cats.shape)
+for i in range(cats.shape[0]):
+    total[i] = df[df['Category'] == cats[i]]['Amt'].sum()
+
+grouped = pd.Series(total, name='Amt', index=pd.CategoricalIndex(cats))
+
+df = pd.concat([budget, grouped], axis=1)
+df = df.dropna(axis=0, how='all')
+fig, ax = plt.subplots()
+df.plot.barh(ax=ax)
+plt.legend()    
